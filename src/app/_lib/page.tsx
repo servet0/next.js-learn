e01c01e0523a0f860031cddbdbf23f650b7ef397
servet0 <servetacar540@gmail.com>
2025-07-04 13:41:44 +0300

    not found page, file colocation, private folders, route groups, layouts, nested layouts, multiple root layouts

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,8 @@
+export default function PrivateRoute() {
+    return (
+        <div>
+            <h1>Private Route</h1>
+            <p>This route is protected and requires authentication.</p>
+        </div>
+    );
+}
